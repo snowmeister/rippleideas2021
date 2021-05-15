93e33036799ac86030b4511b9b5044116f0e58b4
Mark Kennard <mark@threatstatus.com>
2021-05-15 21:42:24 +0100

    Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+/*
+ * /TS/INTERNAL/CODE/CONFIDENTIAL
+ * File: index.js
+ * Project: parcel-react-tailwindcss-template
+ * File Created: Saturday, 15th May 2021 9:09:15 pm
+ * Author: Mark Kennard (mark.kennard@threatstatus.com)
+ * -----
+ * Last Modified: Saturday, 15th May 2021 9:09:15 pm
+ * Modified By: Mark Kennard (mark.kennard@threatstatus.com>)
+ * -----
+ * Copyright 2018 - 2021 Threat Status Limited
+ */
+import React from 'react'
+
+export default function Footer(props) {
+  return (
+    <div className="w-full h-2/8 bg-snowmeister-100">
+      <div className="mt-0 m-4 p-4 text-center text-snowmeister-400 text-shadow-white text-xs">&copy; 2021 - Mark Kennard - All rights reserved</div>
+    </div>
+  )
+}
