ed9943e2bdca5fc1c114affbad4b077127ad02dd
Mark Kennard <mark@threatstatus.com>
2021-05-16 16:27:46 +0100

    Added link to twitter

--- src/components/SocialBar/index.js
+++ src/components/SocialBar/index.js
@@ -11,7 +11,7 @@
  * Copyright 2018 - 2021 Threat Status Limited
  */
 import React from 'react';
-import { TiSocialInstagram, TiSocialLinkedin, TiSocialGithub,TiSocialFlickr } from 'react-icons/ti'
+import { TiSocialInstagram, TiSocialLinkedin, TiSocialGithub,TiSocialTwitter } from 'react-icons/ti'
 import { DiBitbucket, DiCodepen } from 'react-icons/di';
 import { RiMailSendLine } from 'react-icons/ri';
 
@@ -19,6 +19,7 @@ export default function SocialBar() {
   return (
     <div className=" mt-4  flex flex-wrap content-center justify-center">
     
+    <a className="mx-3" target="_blank" rel="noopener noreferrer" href="https://twitter.com/snowy2007"><TiSocialTwitter className="w-8 h-8  text-snowmeister-400 currentColor" /></a>
     <a className="mx-3" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mark-kennard-0480592b/"><TiSocialLinkedin className="w-8 h-8  text-snowmeister-400 currentColor" /></a>
     <a className="mx-3" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/snowyandpixel/"><TiSocialInstagram className="w-8 h-8  text-snowmeister-400 currentColor" /></a>
     <a className="mx-3" target="_blank" rel="noopener noreferrer" href="https://github.com/snowmeister"><TiSocialGithub className="w-8 h-8  text-snowmeister-400 currentColor" /></a>
